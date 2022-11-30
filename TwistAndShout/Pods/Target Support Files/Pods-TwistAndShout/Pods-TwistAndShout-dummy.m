29932f22f9deebc2b8a5a11c5259205cc5fe09be
Ramiro Diaz <ramiro.leandro.diaz@gmail.com>
2022-11-30 01:47:01 -0300

    Project Started + Added pod

---
+++ TwistAndShout/Pods/Target Support Files/Pods-TwistAndShout/Pods-TwistAndShout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TwistAndShout : NSObject
+@end
+@implementation PodsDummy_Pods_TwistAndShout
+@end
---
+++ TwistAndShout/TwistAndShout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwistAndShout
+//
+//  Created by Ramiro Diaz on 30/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
